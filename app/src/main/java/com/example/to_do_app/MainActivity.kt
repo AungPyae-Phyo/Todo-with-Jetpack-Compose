91ed72699828e7ae9a0949156974a5f1961732b8
aungpyaephy0 <pyaea978@gmail.com>
2024-10-13 13:17:25 +0630

    changes

--- app/src/main/java/com/example/to_do_app/MainActivity.kt
+++ app/src/main/java/com/example/to_do_app/MainActivity.kt
@@ -20,6 +20,7 @@ import com.example.to_do_app.ui.theme.ToDoAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContent {
             ToDoAppTheme {
